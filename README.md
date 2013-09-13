83e7336770e587a4f12da1966bad508c28e306e0
mimaison <mickael.maison@gmail.com>
2013-09-13 18:18:24 +0200

    Rewrote the widget. I now depends on mm.itemselector

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # jQuery Month Selector #
 
-This is a jQueryUI Plugin that allows to select a month and year using the accordion widget
+This is a jQueryUI Plugin that allows to select a month and year using the mm.itemselector widget (http://plugins.jquery.com/mm.itemselector/)
 
 ## Usage & Demo ##
 
-https://github.com/mimaison/jQueryUI-Month-Selector
+http://www.mickaelmaison.com/monthselector/
+
+## jQuery Plugin Page ##
+
+http://plugins.jquery.com/mm.monthselector/
 
 ## Version History ##
 
--- mm.monthselector.js
+++ mm.monthselector.js
@@ -3,79 +3,65 @@
  * https://github.com/mimaison/jQueryUI-Month-Selector
  *
  * Depends:
- *	jquery.ui.accordion.js
+ *	mm.itemselector
  */
 (function($) {
-"use strict";
-$.widget("mm.monthselector", {
-	version: "1.0.0",
+'use strict';
+$.widget('mm.monthselector', {
+	version: '1.0',
 
 	options: {
-		names: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+		names: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
 		start: new Date(),
 		end: new Date(),
 		selected: new Date(),
-
-		//Callback
 		callback: null
 	},
 
 	_create: function() {
 		var self = this,
-				endYear = this.options.end.getFullYear(),
-				startYear = this.options.start.getFullYear();
+			endYear = this.options.end.getFullYear(),
+			startYear = this.options.start.getFullYear(),
+			dates = [];
 		for (var i = endYear; i >= startYear; i--) {
 			var maxMonth = (i === endYear) ? this.options.end.getMonth() : this.options.names.length - 1;
 			var minMonth = (i === startYear) ? this.options.start.getMonth() : 0;
-			$("<h3>" + i + "</h3>").appendTo(this.element);
-			var div = $("<div>").appendTo(this.element);
+			
+			var months = [];
 			for (var j = maxMonth; j >= minMonth; j--) {
-				var html = $("<p id='" + this.element.attr("id") + "_" + i + "_" + j + "'>" + this.options.names[j] + "</p>").appendTo(div);
-				html.mousedown(function() {
-					self._select(this);
-				});
+				months.push(this.options.names[j]);
 			}
+			dates.push([ i.toString(), months ]);
 		}
-		$(this.element).accordion({
-			heightStyle: "content"
+
+		var sel = this.options.selected;
+		var len = (endYear === sel.getFullYear()) ? this.options.end.getMonth() : 11;
+		var selMonth = (sel.getMonth() > 6) ? len - sel.getMonth() : Math.abs(sel.getMonth() - len);
+		var selYear = endYear - sel.getFullYear();
+		
+		$(this.element).itemselector({
+			items: dates,
+			selected: [ selYear , selMonth ],
+			callback: self._callback
 		});
-		this._setOption("selected", this.options.selected);
 	},
 
-	_select: function(element) {
-		var parts = $(element).attr("id").split("_");
-		this.options.selected = new Date(parts[1], parts[2]);
-		$(".active").each(function() {
-			$(this).removeClass("active");
-		});
-		$(element).addClass("active");
-		this._setTab(this._getTab(this.options.selected));
-		if (this.options.callback !== null) {
-			this.options.callback(this.options.selected);
-		}
-	},
-
-	_getTab: function(date) {
-		var index = 0;
-		var year = this.options.end.getFullYear();
-		while (date.getFullYear() < year) {
-			index += 1;
-			year -= 1;
-		}
-		return index;
-	},
-
-	_setTab: function(index) {
-		$(this.element).accordion({
-			active: index
-		});
+	_callback: function(element, item) {
+		var callback = element.monthselector('option', 'callback');
+		var names = element.monthselector('option', 'names');
+		if (callback !== null) {
+			var end = element.monthselector('option', 'end');
+			var months = ((end.getFullYear() - parseInt(item[0])) === end.getFullYear()) ? end.getMonth() : 11;
+			var month = (item[1] > 6) ? months - item[1] : Math.abs(item[1] - months);
+			callback(new Date(end.getFullYear() - item[0], month), names);
+        }
 	},
 
 	_setOption: function(key, value) {
-		if (key === "selected") {
-			var elements = $("#" + this.element.attr("id") + "_" + value.getFullYear() + "_" + value.getMonth());
+		if (key === 'selected') {
+			var elements = $('#' + this.element.attr('id') + '_' + value.getFullYear() + '_' + value.getMonth());
 			if (elements.length === 1) {
-				this._select(elements[0]);
+				$(this.element).itemsel({selected: [ value.getFullYear(), value.getMonth() ] });
 			}
 		}
 	}
